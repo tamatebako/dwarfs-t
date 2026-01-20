288cf7a47742a128c3421dfb24511f22bcd63313
Ronald Tse <ronald.tse@ribose.com>
2026-01-20 17:20:39 +0800

    fix(ci): avoid glibc NSS linker errors with static linking
    Change VCPKG_CRT_LINKAGE from static to dynamic and add
    -static-libgcc -static-libstdc++ to VCPKG_CRT_FLAGS.
    This avoids glibc NSS (getaddrinfo, gethostbyname, getpwuid_r)
    linker errors while maintaining static linking of dependencies.

--- vcpkg_triplets/x64-linux.cmake
+++ vcpkg_triplets/x64-linux.cmake
@@ -1,6 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_C_FLAGS "-fPIC")
 set(VCPKG_CXX_FLAGS "-fPIC")
+set(VCPKG_CRT_FLAGS "-static-libgcc -static-libstdc++")
