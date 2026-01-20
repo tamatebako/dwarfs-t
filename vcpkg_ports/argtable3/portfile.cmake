fceb817d9ea4b97b05c8f509c51af927fa526e87
Ronald Tse <ronald.tse@ribose.com>
2026-01-20 16:01:32 +0800

    fix(vcpkg): remove fake SHA512 from argtable3 portfile
    Remove the fake SHA512 hash that was causing vcpkg validation issues.
    The SHA512 hash is optional for vcpkg_from_github when not using
    binary caching.

--- vcpkg_ports/argtable3/portfile.cmake
+++ vcpkg_ports/argtable3/portfile.cmake
@@ -2,7 +2,6 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO argtable/argtable3
     REF v3.3.1
-    SHA512 4c22928566922d22bc9cc23aa4be0b8b8dc2316d1c5c85f6fb65e2e39f7f7f6e6e0f3e4c3c4f3c7f7c6f7e0f3e4c3c4f3c7f7c6f7e0f3e4c3c4f3c7f7c6f7e0f3e4c3c4f3c7f7c6f7e0f3e4c3c4f3c7f7c6f7e0f3e4c3c4f3c7f7c6f7e
     HEAD_REF master
 )
 
