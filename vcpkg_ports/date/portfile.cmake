fefd2af262833eea0490dde2c64fd65b06d1a800
Ronald Tse <ronald.tse@ribose.com>
2026-02-04 17:28:09 +0800

    fix(vcpkg): add overlay port for date to fix MSYS/MinGW builds
    The upstream vcpkg date port uses vcpkg_cmake_config_fixup(CONFIG_PATH CMake)
    for Windows targets, expecting to find CMake config files in the CMake/
    directory. However, for MinGW/MSYS targets, the date library generates
    config files in lib/cmake/date/ instead.
    
    This overlay port:
    - Detects MSYS/MinGW triplets using VCPKG_TARGET_IS_MINGW or
      VCPKG_CMAKE_SYSTEM_NAME STREQUAL "MSYS"
    - Uses CONFIG_PATH "lib/cmake/date" for MSYS/MinGW (matching non-Windows behavior)
    - Keeps CONFIG_PATH CMake for other Windows targets (MSVC)
    
    This fixes the error:
      'D:/a/dwarfs/vcpkg/packages/date_x64-windows-msys/debug/lib/cmake/date' does not exist
    
    Reference: https://github.com/microsoft/vcpkg/issues/36945

---
+++ vcpkg_ports/date/portfile.cmake
@@ -0,0 +1,50 @@
+# Overlay port for date that fixes MSYS/MinGW builds
+# Issue: vcpkg_cmake_config_fixup looks for config files in CMake/ for Windows targets,
+# but MinGW/MSYS generates them in lib/cmake/date/
+# Fix: Use correct CONFIG_PATH for MinGW/MSYS targets
+
+if(VCPKG_TARGET_IS_WINDOWS)
+  message(WARNING
+    "You will need to also install https://raw.githubusercontent.com/unicode-org/cldr/master/common/supplemental/windowsZones.xml into your install location.\n"
+    "See https://howardhinnant.github.io/date/tz.html"
+  )
+endif()
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO HowardHinnant/date
+  REF "v${VERSION}"
+  SHA512 9bffca5c7cfd1769f66bef330fe4ef0ad2512a8afd229ddb4043a4f166741e697c7a5fbdddf29f7157b3fc2c2c2a80fa7cff45078f1d8ab248d3b07e14518fcf
+  HEAD_REF master
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    INVERTED_FEATURES
+    remote-api USE_SYSTEM_TZ_DB
+)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    ${FEATURE_OPTIONS}
+    -DBUILD_TZ_LIB=ON
+)
+
+vcpkg_cmake_install()
+
+# For MSYS/MinGW, the CMake config files are generated in lib/cmake/date/
+# not in CMake/ like other Windows targets
+if(VCPKG_TARGET_IS_MINGW OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "MSYS")
+  vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/date")
+elseif(VCPKG_TARGET_IS_WINDOWS)
+  vcpkg_cmake_config_fixup(CONFIG_PATH CMake)
+else()
+  vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/date")
+endif()
+
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
