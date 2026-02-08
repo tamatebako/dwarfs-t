b2446598c6b155cca0079101decfcd1793b5f8cc
Ronald Tse <ronald.tse@ribose.com>
2026-02-08 15:28:27 +0800

    fix(ci): update MinGW/MSYS2 vcpkg triplets to use dynamic CRT
    - Change VCPKG_CRT_LINKAGE from static to dynamic (matches standard vcpkg MinGW triplets)
    - Change VCPKG_CMAKE_SYSTEM_NAME from MSYS to MinGW for MSYS2 triplet
    - This should fix GTest linking issues with MinGW builds

--- vcpkg_triplets/x64-windows-msys.cmake
+++ vcpkg_triplets/x64-windows-msys.cmake
@@ -2,9 +2,9 @@
 # Uses MSYS2 for native Windows GCC builds
 
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_ENV_PASSTHROUGH PATH)
 
 # Tell vcpkg this is an MSYS environment
-set(VCPKG_CMAKE_SYSTEM_NAME MSYS)
+set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
