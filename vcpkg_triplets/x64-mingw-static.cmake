a2713a6e4a0d9fb803e622940ab2a2c9173caa44
Ronald Tse <ronald.tse@ribose.com>
2026-02-10 13:24:39 +0800

    fix(build): add static C++ library linking flags for MinGW/MSYS2
    - Add -static-libgcc -static-libstdc++ to linker flags
    - This fixes undefined reference errors to __cxxabiv1 vtable symbols
    - These symbols are required for C++ RTTI (Run-Time Type Information)

--- vcpkg_triplets/x64-mingw-static.cmake
+++ vcpkg_triplets/x64-mingw-static.cmake
@@ -9,5 +9,9 @@ set(VCPKG_ENV_PASSTHROUGH PATH)
 set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
 set(VCPKG_CMAKE_SYSTEM_VERSION 10.0)
 
-# Linker flags to ensure console subsystem is used
-set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_EXE_LINKER_FLAGS=-mconsole" "-DCMAKE_MODULE_LINKER_FLAGS=-mconsole")
+# Linker flags to ensure console subsystem is used and static linking
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS
+    "-DCMAKE_EXE_LINKER_FLAGS=-mconsole -static-libgcc -static-libstdc++ -Wl,-Bstatic"
+    "-DCMAKE_MODULE_LINKER_FLAGS=-mconsole -static-libgcc -static-libstdc++ -Wl,-Bstatic"
+    "-DCMAKE_SHARED_LINKER_FLAGS=-static-libgcc -static-libstdc++ -Wl,-Bstatic"
+)
